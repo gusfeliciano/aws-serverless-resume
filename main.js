bf4048c856eb4b753bb4a3719f89c2dabc5e9921
gusfeliciano <gustav213@gmail.com>
2024-07-11 10:41:35 -0600

    Add deployment workflow and update API URL placeholder

--- main.js
+++ main.js
@@ -3,26 +3,17 @@ document.addEventListener('DOMContentLoaded', (event) => {
 });
 
 function updateVisitorCount() {
-    console.log('Updating visitor count...');
-    fetch('https://mzp04i2u44.execute-api.us-east-1.amazonaws.com/prod/visitorcount', {
+    fetch('REPLACE_WITH_API_GATEWAY_URL', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         }
     })
-    .then(response => {
-        console.log('Response status:', response.status);
-        return response.json();
-    })
+    .then(response => response.json())
     .then(data => {
         console.log('Received data:', data);
-        if (data.body) {
-            const bodyData = JSON.parse(data.body);
-            if (bodyData.visitorCount !== undefined) {
-                document.getElementById('website-counter').textContent = bodyData.visitorCount;
-            } else {
-                console.error('Unexpected body format:', bodyData);
-            }
+        if (data.visitorCount !== undefined) {
+            document.getElementById('website-counter').textContent = data.visitorCount;
         } else {
             console.error('Unexpected data format:', data);
         }
